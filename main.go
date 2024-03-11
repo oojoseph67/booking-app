af147a9f3c402f8cd64c9549fa6cc75e487ac7e8
Joseph <joseph@refine.agency>
2024-03-11 11:14:45 +0100

    updated the loop and added if statements

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 	// with this type of declaration i cant assign the data type
 
 	const conferenceTickets = 50
-	var remainingTickets uint = 33
+	var remainingTickets uint = 50
 
 	var bookings []string // slice... the difference is var bookings [50]string
 
@@ -37,40 +37,85 @@ func main() {
 		var lastName string
 		var email string
 		var userTickets uint
+		var city string
 		// ask user for their name
 
 		fmt.Println("Enter your first name: ")
 		fmt.Scan(&firstName)
+		fmt.Println(" ")
 
 		fmt.Println("Enter your last name: ")
 		fmt.Scan(&lastName)
+		fmt.Println(" ")
 
 		fmt.Println("Enter your email: ")
 		fmt.Scan(&email)
+		fmt.Println(" ")
 
 		fmt.Println("Enter your required ticket: ")
 		fmt.Scan(&userTickets)
+		fmt.Println(" ")
 
-		remainingTickets = remainingTickets - userTickets
-		bookings = append(bookings, firstName+" "+lastName)
+		fmt.Println("Enter desired conference location")
+		fmt.Scan(&city)
+		fmt.Println(" ")
 
-		fmt.Printf("thank you %v %v for booking %v tickets, you will receive a confirmation email @ %v\n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicket := userTickets > 0 && userTickets <= remainingTickets
+		isValidCity := city != "Lagos" && city != "Abuja"
 
-		fmt.Printf("slice length: %v\n", len(bookings))
+		if isValidName && isValidEmail && isValidTicket && isValidCity {
 
-		firstNames := []string{}
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-		// range iterates over elements for different data structures (so not only arrays and slices)
-		// underscores (_) are used as blank identifiers
-		for _, bookingElement := range bookings {
-			var names = strings.Fields(bookingElement)
-			firstNames = append(firstNames, names[0])
+			fmt.Printf("thank you %v %v for booking %v tickets, you will receive a confirmation email @ %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+			fmt.Printf("slice length: %v\n", len(bookings))
+
+			firstNames := []string{}
+
+			// range iterates over elements for different data structures (so not only arrays and slices)
+			// underscores (_) are used as blank identifiers
+			for _, bookingElement := range bookings {
+				var names = strings.Fields(bookingElement)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("these are all our bookings: %v\n", bookings)
+			fmt.Printf("these are all the first names: %v\n", firstNames)
+			fmt.Println(" ")
+
+			if remainingTickets == 0 {
+				// end program
+
+				fmt.Println("All tickets have been sold")
+				fmt.Println("our conference is booked out. come back next year.")
+				break
+			}
+		} else {
+			fmt.Println(" ")
+
+			if !isValidName {
+				fmt.Println("first name or last name you entered is too short")
+			}
+
+			if !isValidEmail {
+				fmt.Println("email does contain the @ sign")
+			}
+
+			if !isValidTicket {
+				fmt.Println("number of tickets you entered is invalid")
+			}
+
+			if !isValidCity {
+				fmt.Println("city must be either Lagos or Abuja")
+			}
+
+			fmt.Println(" ")
+			// break
+			continue
 		}
-		fmt.Printf("these are all our bookings: %v\n", bookings)
-		fmt.Printf("these are all the first names: %v\n", firstNames)
-		fmt.Println(" ")
-		fmt.Println(" ")
-		fmt.Println(" ")
 	}
 }
