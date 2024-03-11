a3907fd3e86377c3c38fa8147d4452f7d53f5101
Joseph <joseph@refine.agency>
2024-03-11 13:26:39 +0100

    encapsulate logic with function

--- main.go
+++ main.go
@@ -17,75 +17,107 @@ import (
 	"strings"
 )
 
-func main() {
-	conferenceName := "mcQu33n GO Conference"
-	// with this type of declaration i cant assign the data type
+var conferenceName = "mcQu33n GO Conference"
 
-	const conferenceTickets = 50
-	var remainingTickets uint = 50
+const conferenceTickets = 50
 
-	var bookings []string // slice... the difference is var bookings [50]string
+var remainingTickets uint = 50
+var bookings []string // slice... the difference is var bookings [50]string
 
-	// fmt.Printf("%T, %T, %T\n", conferenceTickets, remainingTickets, conferenceName)
-
-	fmt.Printf("Welcome to %v conferenceName booking application\n", conferenceName)
+func greetUsers() {
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+}
+
+func getFirstNames(bookingDetails []string) []string {
+
+	firstNames := []string{}
+
+	// range iterates over elements for different data structures (so not only arrays and slices)
+	// underscores (_) are used as blank identifiers
+	for _, bookingElement := range bookingDetails {
+		var names = strings.Fields(bookingElement)
+		firstNames = append(firstNames, names[0])
+	}
+
+	return firstNames
+	// fmt.Printf("these are all our bookings: %v\n", bookings)
+	// fmt.Printf("these are all the first names: %v\n", firstNames)
+	// fmt.Println(" ")
+}
+
+func validation(firstName string, lastName string, email string, userTickets uint, city string) (bool, bool, bool, bool) {
+
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicket := userTickets > 0 && userTickets <= remainingTickets
+	isValidCity := city != "Lagos" && city != "Abuja"
+
+	return isValidName, isValidEmail, isValidTicket, isValidCity
+}
+
+func getUserInput() (string, string, string, uint, string) {
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	var city string
+	// ask user for their name
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println(" ")
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println(" ")
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+	fmt.Println(" ")
+
+	fmt.Println("Enter your required ticket: ")
+	fmt.Scan(&userTickets)
+	fmt.Println(" ")
+
+	fmt.Println("Enter desired conference location")
+	fmt.Scan(&city)
+	fmt.Println(" ")
+
+	return firstName, lastName, email, userTickets, city
+
+}
+
+func bookTicket(firstName string, lastName string, userTickets uint, email string, conferenceName string) []string {
+
+	remainingTickets = remainingTickets - userTickets
+	bookingDetails := append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("thank you %v %v for booking %v tickets, you will receive a confirmation email @ %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	return bookingDetails
+
+	// fmt.Printf("slice length: %v\n", len(bookings))
+}
+
+func main() {
+
+	greetUsers()
 
 	for {
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint
-		var city string
-		// ask user for their name
+		firstName, lastName, email, userTickets, city := getUserInput()
 
-		fmt.Println("Enter your first name: ")
-		fmt.Scan(&firstName)
-		fmt.Println(" ")
+		isValidName, isValidEmail, isValidTicket, isValidCity := validation(firstName, lastName, email, userTickets, city)
 
-		fmt.Println("Enter your last name: ")
-		fmt.Scan(&lastName)
-		fmt.Println(" ")
+		if isValidName && isValidEmail && isValidTicket {
 
-		fmt.Println("Enter your email: ")
-		fmt.Scan(&email)
-		fmt.Println(" ")
+			bookingDetails := bookTicket(firstName, lastName, userTickets, email, conferenceName)
 
-		fmt.Println("Enter your required ticket: ")
-		fmt.Scan(&userTickets)
-		fmt.Println(" ")
-
-		fmt.Println("Enter desired conference location")
-		fmt.Scan(&city)
-		fmt.Println(" ")
-
-		isValidName := len(firstName) >= 2 && len(lastName) >= 2
-		isValidEmail := strings.Contains(email, "@")
-		isValidTicket := userTickets > 0 && userTickets <= remainingTickets
-		isValidCity := city != "Lagos" && city != "Abuja"
-
-		if isValidName && isValidEmail && isValidTicket && isValidCity {
-
-			remainingTickets = remainingTickets - userTickets
-			bookings = append(bookings, firstName+" "+lastName)
-
-			fmt.Printf("thank you %v %v for booking %v tickets, you will receive a confirmation email @ %v\n", firstName, lastName, userTickets, email)
-			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-
-			fmt.Printf("slice length: %v\n", len(bookings))
-
-			firstNames := []string{}
-
-			// range iterates over elements for different data structures (so not only arrays and slices)
-			// underscores (_) are used as blank identifiers
-			for _, bookingElement := range bookings {
-				var names = strings.Fields(bookingElement)
-				firstNames = append(firstNames, names[0])
-			}
-			fmt.Printf("these are all our bookings: %v\n", bookings)
+			firstNames := getFirstNames(bookingDetails)
 			fmt.Printf("these are all the first names: %v\n", firstNames)
-			fmt.Println(" ")
 
 			if remainingTickets == 0 {
 				// end program
